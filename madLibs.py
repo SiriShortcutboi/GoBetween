2f6a8b779bef148f606cf3a88f130167c8e19938
SiriShortcutboi <58081965+SiriShortcutboi@users.noreply.github.com>
2021-09-14 08:26:44 -0600

    Add madlibs to windows machine

---
+++ madLibs.py
@@ -0,0 +1,49 @@
+#Holden Anderson
+#9-21
+#madlibs program
+–-
+#text variable
+
+word1=input("enter an place")
+word2=input("enter the name of a place")
+word3=input("enter the name of a place")
+word4=input("enter the name of a place")
+word5=input("enter the name of a place")
+word6=input("enter the name of a place")
+word7=input("enter the name of a place")
+word8=input("enter a noun")
+word9=input("enter the name of a character who is better than another")
+word10=input("enter the name of the inferior character")
+word11=input("enter the name of a place")
+word12=input("enter the name of a place")
+word13=input("enter the name of a place")
+word14=input("enter the name of a place")
+word15=input("enter the name of a place")
+word16=input("enter the name of a place")
+word17=input("enter the name of a place")
+word18=input("enter the name of a place")
+word19=input("enter the name of a place")
+word20=input("enter the name of a place")
+
+text="""
+"Hero of """+word1+""", Beautiful is better than """+word2.+"""
+Explicit is"""+word3+"""than implicit.
+Simple is better than """+word4+."""
+"""+word5+""" is better than complicated.
+Flat is better than verb.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although """+word10+""" beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+"""
+print(text)
